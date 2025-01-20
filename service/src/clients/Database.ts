cbd6b1ec9b71ac39cc2b25a27ae979dadfc0fdb2
Daniel Rowe <danielrowetech@gmail.com>
2025-01-20 14:35:46 -0800

    update db configuration options

--- service/src/clients/Database.ts
+++ service/src/clients/Database.ts
@@ -1,8 +1,11 @@
 import {MongoClient} from "mongodb";
 
-const HOST = process.env.NODE_ENV === 'production'
-    ? 'database'
-    : 'localhost';
+const ENV_HOSTS = {
+    production: 'http://p2piano.home',
+    compose: 'database',
+} as const;
+
+const HOST = ENV_HOSTS[process.env.NODE_ENV] ?? 'localhost';
 const URI = `mongodb://root:password@${HOST}:27017`;
 const client = new MongoClient(URI);
 
