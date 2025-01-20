d574ede1092ab2b605132fb387f6afa9fffe41c6
Daniel Rowe <danielrowetech@gmail.com>
2025-01-20 15:19:22 -0800

    Require DB_URL env var

--- service/src/clients/Database.ts
+++ service/src/clients/Database.ts
@@ -1,11 +1,7 @@
 import {MongoClient} from "mongodb";
 
-const ENV_HOSTS = {
-    production: 'http://p2piano-db.home',
-    compose: 'database',
-} as const;
 
-const HOST = ENV_HOSTS[process.env.NODE_ENV] ?? 'localhost';
+const HOST = process.env.DB_URL ?? 'localhost';
 const URI = `mongodb://root:password@${HOST}:27017`;
 const client = new MongoClient(URI);
 
